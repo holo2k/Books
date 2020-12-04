dc943426beefa4f3ac09e723a548b8ae3e48e566
iamHolo <nneketaa@yandex.ru>
2020-12-04 16:07:52 +0500

    Добавьте файлы проекта.

---
+++ Books/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books
+{
+    public class Book
+    {
+        internal string AuthorName;
+        internal string Title;
+        public string CodeOfBook;
+        public int PagesCount;
+        public bool Collectable;
+
+        public Book(string CodeOfBook, int PagesCount, bool Collectable)
+        {
+            AuthorName = "Неизвестно";
+            Title = "Неизвестно";
+            this.CodeOfBook = CodeOfBook;
+            this.PagesCount = PagesCount;
+            this.Collectable = Collectable;
+        }
+
+        public Book(string AuthorName, string Title, string CodeOfBook, int PagesCount, bool Collectable)
+        {
+            this.AuthorName = AuthorName;
+            this.Title = Title;
+            this.CodeOfBook = CodeOfBook;
+            this.PagesCount = PagesCount;
+            this.Collectable = Collectable;
+        }
+
+    }
+}
---
+++ Books/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book Book1 = new Book("365-8958-4783", 248, true);
+            Book Book2 = new Book("Владимир Маяковский", "Я сам", "978-5-4453-0641-2", 224, false);
+
+            bool isCollectable1 = Book1.Collectable;
+            bool isCollectable2 = Book2.Collectable;
+
+            string collectable1;
+            string collectable2;
+
+            if (isCollectable1)
+            {
+                collectable1 = "Коллекционное";
+            }
+            else
+            {
+                collectable1 = "Не коллекционное";
+            }
+
+            if (isCollectable2)
+            {
+                collectable2 = "Коллекционное";
+            }
+            else
+            {
+                collectable2 = "Не коллекционное";
+            }
+
+            Console.WriteLine($"{Book2.AuthorName} \n" +
+                              $"{Book2.Title} \n" +
+                              $"{Book2.CodeOfBook} \n" +
+                              $"{Book2.PagesCount} \n" +
+                              $"{collectable2}\n");
+
+            Console.WriteLine($"{Book1.AuthorName} \n" +
+                              $"{Book1.Title} \n" +
+                              $"{Book1.CodeOfBook} \n" +
+                              $"{Book1.PagesCount} \n" +
+                              $"{collectable1}");
+            Console.ReadLine();
+
+        }
+    }
+}
